779bfc302b29b109e0c82bf79adf5025cb6da2b6
test666v2 <test666v2@gmail.com>
2017-04-21 22:57:01 +0100

    Update chaff.sh

--- chaff.sh
+++ chaff.sh
@@ -3,15 +3,15 @@
 #
 # chaff.sh
 #
-# Purpose: Add additional pseudo-randomness to /dev/random from 4 sources : date, iostat, temperature (please read ahead) and ping
+# Purpose: Add additional pseudo-randomness to /dev/random from 4 sources : date, iostat, temperature (please verify ahead in the script) and ping
 #
-# please install sensors package or set variable GET_ENTROPY_FROM_TEMPERATURE to false
+# Please install sensors package or set variable GET_ENTROPY_FROM_TEMPERATURE to false
 #
-# the terms "random" and "randomness" are not used here as meaning "truely random" or "true randomness"
+# The terms "random", "randomized" and "randomness" are here loosely used and do not mean "true randomness"
 #
-# better to have a reliable hardware RNG or install haveged and be less happy, or to be a tin-foil hat moron and run chaff.sh alongside haveged
+# It would be better to have a reliable hardware RNG or install haveged and be less happy, or both, or to be a tin-foil hat moron and run chaff.sh alongside haveged
 #
-# in a terminal window: "chmod +x /your/path/here/chaff.sh" to make the script executable
+# In a terminal window: "chmod +x /your/path/here/chaff.sh" to make the script executable
 #
 # run from cron: edit, as root, cron in a terminal window with "crontab -e" and add "@reboot /your/path/here/chaff.sh", save and exit and reboot
 #
@@ -48,6 +48,7 @@
 #
 # Starting values for some variables
 #
+ALPHA_STRING_TEMP_FILE=/dev/shm/.ALPHA_STRING_TEMP_FILE
 CPU_DURESS_MAX=19 # 21 and more is way over the top # used to "disturb" possible predicatibility in temperature readings
 CPU_DURESS_MIN=11 # 10 or less will probably be irrelevant # used to "disturb" possible predicatibliity in temperature readings
 DELAY_MAX=20 # in seconds # MAX interval between script looping
@@ -61,28 +62,30 @@ NUMBER_OF_CORES=$(( $(grep -m 1 -i cores < /proc/cpuinfo | awk '{print $4}') -1 
 NUMBER_OF_PINGS=4
 PING_INTERVAL=0.2 # 0.2 for interval between pings, have to be root for lower value
 RANDOM_CHARS_FILE="/dev/shm/.random.hex" # or whatever you want, perhaps /tmp/.random.hex ?
-RANDOM_ROUNDS_MAX=10 # number of loops for sequential checksums from date (time), iostat, sensors (temperature) and ping # perhaps their order should/could be randomized
+RANDOM_ROUNDS_MAX=10 # number of loops for sequential checksums from date (time), iostat, sensors (temperature) and ping # randomized order of execution
 RANDOM_ROUNDS_MIN=1 # 0 is poinless, but may introduce some "randomness"
 #
 #########################################################
 #
 entropy_from_date ()
 {
-### date (time) in nanoseconds
-ALPHA_STRING+=$(/bin/date +%s%N | /usr/bin/shasum -a 512 | /usr/bin/awk '{print $1}') # get only shasum from column 1
+   [ $GET_ENTROPY_FROM_DATE ] || return
+   ALPHA_STRING+=$(/bin/date +%s%N | /usr/bin/shasum -a 512 | /usr/bin/awk '{print $1}') # get only shasum from column 1
 }
 #
 #########################################################
 #
 entropy_from_iostat ()
 {
-ALPHA_STRING+=$(/usr/bin/iostat | /usr/bin/shasum -a 512 | /usr/bin/awk '{print $1}') # get only shasum from column 1
+   [ $GET_ENTROPY_FROM_IOSTAT ] || return
+   ALPHA_STRING+=$(/usr/bin/iostat | /usr/bin/shasum -a 512 | /usr/bin/awk '{print $1}') # get only shasum from column 1
 }
 #
 #########################################################
 #
 entropy_from_temperature () ### probably way over the top
 {
+   [ $GET_ENTROPY_FROM_TEMPERATURE ] || return
    for (( CORE = 0; CORE <= NUMBER_OF_CORES; CORE++ )) # launch bc up to $NUMBER_OF_CORES + 1 threads, so for 4 cores we get 4 "simultaneous" threads
       do
          CPU_DURESS=$(( ( RANDOM % (( CPU_DURESS_MAX - CPU_DURESS_MIN + 1 )) )  + CPU_DURESS_MIN ))
@@ -103,8 +106,8 @@ entropy_from_temperature () ### probably way over the top
 #
 entropy_from_ping ()
 {
-### pings to $IP_TO_PING
-ALPHA_STRING+=$(/bin/ping -i $PING_INTERVAL -c $NUMBER_OF_PINGS $IP_TO_PING | /usr/bin/shasum -a 512 | /usr/bin/awk '{print $1}') # get only shasum from column 1
+   [ $GET_ENTROPY_FROM_PING ] || return
+   ALPHA_STRING+=$(/bin/ping -i $PING_INTERVAL -c $NUMBER_OF_PINGS $IP_TO_PING | /usr/bin/shasum -a 512 | /usr/bin/awk '{print $1}') # get only shasum from column 1
 }
 #
 #########################################################
@@ -114,22 +117,31 @@ ALPHA_STRING+=$(/bin/ping -i $PING_INTERVAL -c $NUMBER_OF_PINGS $IP_TO_PING | /u
 while true
    do
       ENTROPY_GATHERING_ROUNDS=$(( ( RANDOM % (( RANDOM_ROUNDS_MAX - RANDOM_ROUNDS_MIN + 1 )) )  + RANDOM_ROUNDS_MIN ))
-      ALPHA_STRING=""
-      for (( i  = 1; i <= ENTROPY_GATHERING_ROUNDS; i++ )) # generate up to 4 checksums from date (time), iostat, sensors (temperature) and ping
+      for (( i  = 1; i <= ENTROPY_GATHERING_ROUNDS; i++ )) # generate up to $RANDOM_ROUNDS_MAX checksums from date (time), iostat, sensors (temperature) and ping
          do
-            [ ! $GET_ENTROPY_FROM_DATE ] || entropy_from_date
-            [ ! $GET_ENTROPY_FROM_IOSTAT ] || entropy_from_iostat
-            [ ! $GET_ENTROPY_FROM_TEMPERATURE ] || entropy_from_temperature
-            [ ! $GET_ENTROPY_FROM_PING ] || entropy_from_ping
+            WHAT_TO_DO=$(/usr/bin/printf "1\n2\n3\n4" | shuf)
+            echo "$WHAT_TO_DO" | \
+               while read -r TO_DO
+                  do
+                    case "$TO_DO" in
+                        1) entropy_from_date ;;
+                        2) entropy_from_iostat ;;
+                        3) entropy_from_temperature ;;
+                        4) entropy_from_ping
+                     esac
+                  done
+            /usr/bin/printf "$ALPHA_STRING" >> $ALPHA_STRING_TEMP_FILE # because $ALPHA_STRING will be destroyed on leaving the pipe or loop
          done
-         BETA_STRING=$(echo -n "$ALPHA_STRING" | fold -w1 | shuf | shuf | tr -d '\n' | fold -w2) # break ALPHA_STRING string in 1 character lines, mix twice (shuf), remove line break, get a single line and break again but in 2 characters lines
-         /bin/cat /dev/null > $RANDOM_CHARS_FILE # generate an empty $RANDOM_CHARS_FILE
-         echo "$BETA_STRING" | \
-            while read -r HEXOR
-               do
-                  /usr/bin/printf "\x$HEXOR" >> $RANDOM_CHARS_FILE # printf each line as hexadecimal coded characters to $RANDOM_CHARS_FILE
-               done
+#      BETA_STRING=$(cat $ALPHA_STRING_TEMP_FILE | /usr/bin/fold -w1 | /usr/bin/shuf | /usr/bin/tr -d '\n' | /usr/bin/fold -w2) # break $ALPHA_STRING_TEMP_FILE string in 1 character lines, mix (shuf), remove line break,  break again but in 2 characters lines
+BETA_STRING=$(/usr/bin/fold -w1 < $ALPHA_STRING_TEMP_FILE | /usr/bin/shuf | /usr/bin/tr -d '\n' | /usr/bin/fold -w2) # break $ALPHA_STRING_TEMP_FILE string in 1 character lines, mix (shuf), remove line break,  break again but in 2 characters lines
+      echo "$BETA_STRING" | \
+         while read -r HEXOR
+            do
+               /usr/bin/printf "\x$HEXOR" >> $RANDOM_CHARS_FILE # printf each line as hexadecimal coded characters to $RANDOM_CHARS_FILE
+            done
       /bin/cat $RANDOM_CHARS_FILE > /dev/random # feed $RANDOM_CHARS_FILE to the /dev/random pool
+      /usr/bin/printf "" > $ALPHA_STRING_TEMP_FILE # generate an empty ALPHA_STRING_TEMP_FILE # more than one way to skin a cat
+      /bin/cat /dev/null > $RANDOM_CHARS_FILE # generate an empty $RANDOM_CHARS_FILE # more than one way to skin a cat
       DELAY=$(( ( RANDOM % (( DELAY_MAX - DELAY_MIN + 1 )) )  + DELAY_MIN ))
       /bin/sleep $DELAY
    done
