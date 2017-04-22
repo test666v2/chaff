0e13aadd4e82206bf79fe29bd7dd036bd98f92bf
test666v2 <test666v2@gmail.com>
2017-04-22 23:42:00 +0100

    Update chaff.sh

--- chaff.sh
+++ chaff.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-####################################################
-#
+###################################################
+
 # chaff.sh
-#
+
 # Purpose: Add additional pseudo-randomness to /dev/random from 4 sources : date, iostat, temperature (please verify ahead in the script) and ping
-#
-# Please install sensors package or set variable GET_ENTROPY_FROM_TEMPERATURE to false
-#
+
+# Please install bc, sensors and systat packages
+
 # The terms "random", "randomized" and "randomness" are here loosely used and do not mean "true randomness"
-#
+
 # It would be better to have a reliable hardware RNG or install haveged and be less happy, or both, or to be a tin-foil hat moron and run chaff.sh alongside haveged
-#
+
 # In a terminal window: "chmod +x /your/path/here/chaff.sh" to make the script executable
-#
+
 # run from cron: edit, as root, cron in a terminal window with "crontab -e" and add "@reboot /your/path/here/chaff.sh", save and exit and reboot
-#
+
 # EARLY ALPHA - YOU HAVE BEEN WARNED
-#
-####################################################
-#
+
+###################################################
+
 # DISCLAIMER
-#
+
 # Use this script at your own risk
 # You, as a user, have no right to support even if implied
 # Carefully read the script and then interpret, modify, correct, fork, disdain, whatever
-#
-####################################################
-#
+
+###################################################
+
 #Copyright (c) <2017> <test666v2>
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -43,20 +43,20 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
-#
-####################################################
-#
+
+###################################################
+
 # Starting values for some variables
-#
-ALPHA_STRING_TEMP_FILE=/dev/shm/.ALPHA_STRING_TEMP_FILE
+
+ALPHA_STRING_SAVE_FILE=/dev/shm/.ALPHA_STRING_SAVE_FILE
 CPU_DURESS_MAX=19 # 21 and more is way over the top # used to "disturb" possible predicatibility in temperature readings
-CPU_DURESS_MIN=11 # 10 or less will probably be irrelevant # used to "disturb" possible predicatibliity in temperature readings
+CPU_DURESS_MIN=11 # 10 or less will probably be irrelevant # used to "disturb" possible predicatibility in temperature readings
 DELAY_MAX=20 # in seconds # MAX interval between script looping
 DELAY_MIN=5 # in seconds # MIN interval between script looping
 GET_ENTROPY_FROM_DATE=true
 GET_ENTROPY_FROM_IOSTAT=true
 GET_ENTROPY_FROM_PING=true
-GET_ENTROPY_FROM_TEMPERATURE=true # controversial in this implementation, may lead to CPU instability because of random "spikiness" caused by bc # set to "false" to avoid this
+GET_ENTROPY_FROM_TEMPERATURE=true # controversial; may lead to CPU instability because of random "spikiness" caused by bc # set to "false" to avoid this
 IP_TO_PING=0 # 0 is 127.0.0.1 # any other IP will introduce possible unwanted delays, but will probably increase "randomness" # do not ping public IPs
 NUMBER_OF_CORES=$(( $(grep -m 1 -i cores < /proc/cpuinfo | awk '{print $4}') -1 )) # for 4 cores the NUMBER_OF_CORES will be 3 # needed to create temperature increase while spiking CPUs
 NUMBER_OF_PINGS=4
@@ -64,25 +64,25 @@ PING_INTERVAL=0.2 # 0.2 for interval between pings, have to be root for lower va
 RANDOM_CHARS_FILE="/dev/shm/.random.hex" # or whatever you want, perhaps /tmp/.random.hex ?
 RANDOM_ROUNDS_MAX=10 # number of loops for sequential checksums from date (time), iostat, sensors (temperature) and ping # randomized order of execution
 RANDOM_ROUNDS_MIN=1 # 0 is poinless, but may introduce some "randomness"
-#
-#########################################################
-#
+
+########################################################
+
 entropy_from_date ()
 {
    [ $GET_ENTROPY_FROM_DATE ] || return
    ALPHA_STRING+=$(/bin/date +%s%N | /usr/bin/shasum -a 512 | /usr/bin/awk '{print $1}') # get only shasum from column 1
 }
-#
-#########################################################
-#
+
+########################################################
+
 entropy_from_iostat ()
 {
    [ $GET_ENTROPY_FROM_IOSTAT ] || return
    ALPHA_STRING+=$(/usr/bin/iostat | /usr/bin/shasum -a 512 | /usr/bin/awk '{print $1}') # get only shasum from column 1
 }
-#
-#########################################################
-#
+
+########################################################
+
 entropy_from_temperature () ### probably way over the top
 {
    [ $GET_ENTROPY_FROM_TEMPERATURE ] || return
@@ -101,19 +101,19 @@ entropy_from_temperature () ### probably way over the top
          done
    ALPHA_STRING+=$(/usr/bin/sensors -u | /usr/bin/shasum -a 512 | /usr/bin/awk '{print $1}') # poll temperatures from sensors, get only shasum from column 1
 }
-#
-#########################################################
-#
+
+########################################################
+
 entropy_from_ping ()
 {
    [ $GET_ENTROPY_FROM_PING ] || return
    ALPHA_STRING+=$(/bin/ping -i $PING_INTERVAL -c $NUMBER_OF_PINGS $IP_TO_PING | /usr/bin/shasum -a 512 | /usr/bin/awk '{print $1}') # get only shasum from column 1
 }
-#
-#########################################################
-#
+
+########################################################
+
 # MAIN
-#
+
 while true
    do
       ENTROPY_GATHERING_ROUNDS=$(( ( RANDOM % (( RANDOM_ROUNDS_MAX - RANDOM_ROUNDS_MIN + 1 )) )  + RANDOM_ROUNDS_MIN ))
@@ -130,17 +130,16 @@ while true
                         4) entropy_from_ping
                      esac
                   done
-            /usr/bin/printf "$ALPHA_STRING" >> $ALPHA_STRING_TEMP_FILE # because $ALPHA_STRING will be destroyed on leaving the pipe or loop
+            /usr/bin/printf "$ALPHA_STRING" >> $ALPHA_STRING_SAVE_FILE # because $ALPHA_STRING will be destroyed on leaving the pipe or loop
          done
-#      BETA_STRING=$(cat $ALPHA_STRING_TEMP_FILE | /usr/bin/fold -w1 | /usr/bin/shuf | /usr/bin/tr -d '\n' | /usr/bin/fold -w2) # break $ALPHA_STRING_TEMP_FILE string in 1 character lines, mix (shuf), remove line break,  break again but in 2 characters lines
-BETA_STRING=$(/usr/bin/fold -w1 < $ALPHA_STRING_TEMP_FILE | /usr/bin/shuf | /usr/bin/tr -d '\n' | /usr/bin/fold -w2) # break $ALPHA_STRING_TEMP_FILE string in 1 character lines, mix (shuf), remove line break,  break again but in 2 characters lines
+BETA_STRING=$(/usr/bin/fold -w1 < $ALPHA_STRING_SAVE_FILE | /usr/bin/shuf | /usr/bin/tr -d '\n' | /usr/bin/fold -w2) # break output from $ALPHA_STRING_SAVE_FILE in 1 character lines, mix (shuf), remove line break,  break again but in 2 characters lines
       echo "$BETA_STRING" | \
          while read -r HEXOR
             do
                /usr/bin/printf "\x$HEXOR" >> $RANDOM_CHARS_FILE # printf each line as hexadecimal coded characters to $RANDOM_CHARS_FILE
             done
       /bin/cat $RANDOM_CHARS_FILE > /dev/random # feed $RANDOM_CHARS_FILE to the /dev/random pool
-      /usr/bin/printf "" > $ALPHA_STRING_TEMP_FILE # generate an empty ALPHA_STRING_TEMP_FILE # more than one way to skin a cat
+      /usr/bin/printf "" > $ALPHA_STRING_SAVE_FILE # generate an empty ALPHA_STRING_SAVE_FILE # more than one way to skin a cat
       /bin/cat /dev/null > $RANDOM_CHARS_FILE # generate an empty $RANDOM_CHARS_FILE # more than one way to skin a cat
       DELAY=$(( ( RANDOM % (( DELAY_MAX - DELAY_MIN + 1 )) )  + DELAY_MIN ))
       /bin/sleep $DELAY
